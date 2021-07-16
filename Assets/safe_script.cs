c8834fd134cdb64c7b27c14d1b76285590243fe2
shido0131 <shido.koh@gmail.com>
2021-07-16 18:42:56 +0900

    2021/07/09

---
+++ Assets/safe_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class safe_script : MonoBehaviour
+{
+    bool anima;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anima = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("safe", anima);
+        
+    }
+}
