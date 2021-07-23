515783cdf74c17474a99d93e08030efdf65a1608
shido0131 <shido.koh@gmail.com>
2021-07-23 19:11:22 +0900

    2021/07/23
    次回はクリックした時ダイアルを中心にしてマウスの座標に応じて回す

--- Assets/main_script.cs
+++ Assets/main_script.cs
@@ -4,34 +4,68 @@ using UnityEngine;
 
 public class main_script : MonoBehaviour
 {
-    private float speed = 7;
-    private int number;
-    private Vector3 setTapPos;
-    private Vector3 setMouthPos;
-    // Start is called before the first frame update
+    public AudioClip Sound1;
+    AudioSource audioSource;
+    int number;
+    int pass1;
+    int pass2;
+    int pass3;
+    int pass4;
+    int pass5;
+    int pass6;
+    int pass7;
+    int pass8;
+    int pass9;
+    int pass10;
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
+        number = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, 10.0f))
         {
-            setTapPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (hit.collider.gameObject.tag == "dial")
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (number <= 0)
+                    {
+                        number =60;
+                    }
+                    else
+                    {
+                        number -= 6;
+                    }
+
+                    transform.Rotate(0f, 0f, -6f);
+                    audioSource.PlayOneShot(Sound1);
+                    Debug.Log(number);
+                }
+                else
+                    if (Input.GetMouseButtonDown(1))
+                {
+                    if (number >= 60)
+                    {
+                        number = 0;
+                    }
+                    else
+                    {
+                        number += 6;
+                    }
+                    transform.Rotate(0f, 0f, 6f);
+                    audioSource.PlayOneShot(Sound1);
+                    Debug.Log(number);
+
+                }
+            }
         }
-        // ボタンを押しているとき
-        if (Input.GetMouseButton(0))
-        {
-            setTapPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            setTapPos.z = 10;
-            transform.position = Vector3.Lerp(transform.position, setTapPos, speed * Time.deltaTime);
-            Debug.Log(setTapPos.z);
-        }
-        /*if (Input.GetMouseButton(0))
-        {
-            setTapPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }*/
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red, 5);
+        
     }
 }
