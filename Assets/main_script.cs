9a66549369d7d094562788e0e3c98a1f042003b9
shido0131 <shido.koh@gmail.com>
2021-10-22 19:06:28 +0900

    2021/10/22,19:06
    次回は音声のバグを直す

--- Assets/main_script.cs
+++ Assets/main_script.cs
@@ -1,26 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
 
 public class main_script : MonoBehaviour
 {
-    public float Volume;
+    float Volume;
     public AudioClip Sound1;
-    AudioSource audioSource;
+    public AudioSource audioSource;
     public GameObject dummy_safe;
     public GameObject clear_safe;
     public Text nownuber;
-    int soundlevel;
     int number;
-    int difference;
     int passtimes;
     int dummyonthewayMousePosition;
-    public float stoptime;
-    public int pass;
+    float stoptime;
+    int pass;
     private Camera mainCamera;
     private float startMousePosition;
     private float lastMousePosition;
     private float onthewayMousePosition;
     private bool measurement;
+    private bool delay;
 
     void Start()
     {
@@ -36,19 +37,6 @@ public class main_script : MonoBehaviour
 
 
     }
-    void CalcAnswer()
-    {
-        difference = pass - number;
-        if (difference < 0)
-        {
-            difference = difference * -1;
-        }
-    }
-    void gameclear()
-    {
-
-    }
-
 
     // Update is called once per frame
     void Update()
@@ -70,7 +58,7 @@ public class main_script : MonoBehaviour
             Debug.DrawRay(ray.origin, ray.direction * 10, Color.red, 5);
 
         }
-        if (Input.GetMouseButton(0)&&measurement==true)
+        if (Input.GetMouseButton(0))
         {
             lastMousePosition = Input.mousePosition.x;
             onthewayMousePosition = (lastMousePosition - startMousePosition) / 10;//ダイアルをクリックした時の座標と離した時の差分
@@ -78,7 +66,7 @@ public class main_script : MonoBehaviour
             nownuber.text = ((int)onthewayMousePosition).ToString();
             nownuber.color = new Color(0.9922f, 0.4941f, 0f, 1f);
         }
-        if (Input.GetMouseButtonUp(0) && measurement == true)
+        if (Input.GetMouseButtonUp(0) && measurement == true && delay == false)
         {
             measurement = false;
             number += (int)onthewayMousePosition;
@@ -99,12 +87,8 @@ public class main_script : MonoBehaviour
             {
                 dummyonthewayMousePosition *= -1;
             }
-            for (int i=0;i<dummyonthewayMousePosition;++i)
-            {
-                
-                transform.Rotate(0f,0f,6f);
-            }
-            transform.Rotate(0f,0f,onthewayMousePosition*6);
+            StartCoroutine("Dealay");
+            //transform.Rotate(0f,0f,onthewayMousePosition*6);
             nownuber.text = (number).ToString();
             nownuber.color = new Color(1f, 0.92f, 0.016f, 1f);
             //ダイアルをクリックした時の座標と離した時の差分を角度回す●
@@ -121,7 +105,7 @@ public class main_script : MonoBehaviour
                 pass = Random.Range(1, 60);
                 if (passtimes == 0)
                 {
-                    GetComponent<main_script>().gameclear();
+                    //GetComponent<main_script>().gameclear();
                 }
 
             }
@@ -130,5 +114,24 @@ public class main_script : MonoBehaviour
                 stoptime = 0;
             }
         }
+    }
+    public IEnumerator Dealay()
+    {
+        delay = true;
+        for (int i = 0; i < dummyonthewayMousePosition; ++i)
+        {
+            audioSource.PlayOneShot(Sound1);
+            if (onthewayMousePosition < 0)
+            {
+                transform.Rotate(0f, 0f, -6f);
+            }
+            else
+            {
+                transform.Rotate(0f, 0f, 6f);
+            }
+            
+            yield return new WaitForSeconds(0.1f/onthewayMousePosition);
+        }
+        delay = false;
     }
 }
